252cb32f4d6352ee3aac270e2c64867117b150ab
Abrahan Silverio <abrahansmg@gmail.com>
2019-04-20 04:34:28 -0400

    added find icon on transfer 3 form

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,23 @@
 (function ($){
 
   // swear agreement
-  $('#agree-date, #birth-date').each(function (i, input){
+  $('#agree-date, #birth-date').each((i, input) => {
     $(input).datepicker({
       language: 'es',
     })
+  })
+
+  // edit btn action
+  $('.section__edit-btn').on('click', (e) => {
+    $('#client-name').prop('disabled', false)
+    $(e.target).prop('disabled', true)
+
+    alert('Edit Callback')
+  })
+
+  // find licence callback
+  $('.find-licence').on('click', (e) => {
+    alert('Find Licence Callback')
   })
 
 })(jQuery)
